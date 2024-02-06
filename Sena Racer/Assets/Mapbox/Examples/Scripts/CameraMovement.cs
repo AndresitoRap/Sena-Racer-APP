94a63ee691e42ca072d66ba168d363095dbf5117
AndresitoRap <andresfelipecvs@hotmail.com>
2024-02-06 00:34:09 -0500

    Fix Bugs

--- Sena Racer/Assets/Mapbox/Examples/Scripts/CameraMovement.cs
+++ Sena Racer/Assets/Mapbox/Examples/Scripts/CameraMovement.cs
@@ -36,20 +36,20 @@ namespace Mapbox.Examples
 					break;
 				case 2:
 					{
-						// Store both touches.
-						Touch touchZero = Input.GetTouch(0);
-						Touch touchOne = Input.GetTouch(1);
+						// // Store both touches.
+						// Touch touchZero = Input.GetTouch(0);
+						// Touch touchOne = Input.GetTouch(1);
 
-						// Find the position in the previous frame of each touch.
-						Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-						Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+						// // Find the position in the previous frame of each touch.
+						// Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+						// Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-						// Find the magnitude of the vector (the distance) between the touches in each frame.
-						float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-						float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+						// // Find the magnitude of the vector (the distance) between the touches in each frame.
+						// float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+						// float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
-						// Find the difference in the distances between each frame.
-						zoomFactor = 0.05f * (touchDeltaMag - prevTouchDeltaMag);
+						// // Find the difference in the distances between each frame.
+						// zoomFactor = 0.05f * (touchDeltaMag - prevTouchDeltaMag);
 					}
 					ZoomMapUsingTouchOrMouse(zoomFactor);
 					break;
